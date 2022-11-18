9f1cb324bc144f7d07bb41f05ae5e884c8fbc245
Raul França <raulmfranca@gmail.com>
2022-11-17 23:47:12 -0300

    add infoLog e errorLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,10 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// http.NewServeMux() inicializa um novo servemux
-	mux := http.NewServeMux()
 
 	// Define uma flag de linha de comando com o nome 'addr', um valor padrão de ":4000"
 	//e um texto ajuda explicando o que o flag controla
@@ -17,6 +16,15 @@ func main() {
 	//no path root $ go run . -addr=":8000"
 	flag.Parse()
 
+	//log.New() criar um logger para escrever mensagens de informação.
+	//Parâmetros: o destino para gravar os logs (os.Stdout), uma string
+	//prefixo para mensagem "INFO\t",  log.Ldate|log.Ltime
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// Criar message de error segue a mesma fórmula; add |log.Lshortfile para indicar o arq
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// http.NewServeMux() inicializa um novo servemux
+	mux := http.NewServeMux()
 	// Registra home function como handler para "/" URL
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -28,9 +36,10 @@ func main() {
 
 	// http.ListenAndServe() function inicia um new web server. Recebe 2 parâmetros:
 	// TCP network address to listen on (no caso ":4000"),e o servemux
-	log.Printf("Starting server on %s", *addr)
+	//log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
-		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 }
