25f965dfd5614e80f493558908a553ad1f735829
Raul França <raulmfranca@gmail.com>
2022-11-17 23:00:58 -0300

    add command-line flag addr

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,14 @@ import (
 func main() {
 	// http.NewServeMux() inicializa um novo servemux
 	mux := http.NewServeMux()
+
+	// Define uma flag de linha de comando com o nome 'addr', um valor padrão de ":4000"
+	//e um texto ajuda explicando o que o flag controla
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//A função flag.Parse() le se a flag foi usada no command-line e altera seu valor se usada
+	//no path root $ go run . -addr=":8000"
+	flag.Parse()
+
 	// Registra home function como handler para "/" URL
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -19,8 +28,8 @@ func main() {
 
 	// http.ListenAndServe() function inicia um new web server. Recebe 2 parâmetros:
 	// TCP network address to listen on (no caso ":4000"),e o servemux
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
