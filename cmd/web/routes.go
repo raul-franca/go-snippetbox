88d5d9df728694c23fabfc4bb44fb9dac2f2acef
Raul França <raulmfranca@gmail.com>
2022-11-21 23:24:04 -0300

    correcoes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,9 +4,8 @@ import "net/http"
 
 func (app application) routes() *http.ServeMux {
 
-	// http.NewServeMux() inicializa um novo servemux
 	mux := http.NewServeMux()
-	// Registra home function como handler para "/" URL
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
